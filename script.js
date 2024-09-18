0b43ca0bf93039160dedb3bd61478eb16ba66cfd
danielz0102 <danyzglez@hotmail.com>
2024-09-17 19:58:13 -0600

    Add playRound function to play just one round

--- script.js
+++ script.js
@@ -3,24 +3,43 @@ let humanScore = 0, computerScore = 0
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3)
     
-    return choice === 0 ? 'Rock' :
-        choice === 1 ? 'Paper' :
-        'Scissors'
+    return choice === 0 ? 'ROCK' :
+        choice === 1 ? 'PAPER' :
+        'SCISSORS'
 }
 
 function getHumanChoice() {
-    let choice = prompt('Enter an option: Rock, Paper or Scissors').toLocaleLowerCase().trim()
+    let choice = prompt('Enter an option: Rock, Paper or Scissors').toUpperCase().trim()
 
-    if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
-        alert('Sorry, that\'s not correct. Please, choice rock, paper or scissors.')
-        return 0
+    if (choice !== 'ROCK' && choice !== 'PAPER' && choice !== 'SCISSORS') {
+        console.error('Sorry, that\'s not correct. Please, choice rock, paper or scissors.')
+        getHumanChoice()
     }
 
     return choice
 }
 
 function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'ROCK' && computerChoice === 'SCISSORS'
+        || humanChoice === 'PAPER' && computerChoice === 'ROCK'
+        || humanChoice === 'SCISSORS' && computerChoice === 'PAPER'
+    ) {
+        console.log(`${humanChoice} vs ${computerChoice}. You Win!`)
+        humanScore++
+    }
+    else if (humanChoice === 'ROCK' && computerChoice === 'PAPER'
+        || humanChoice === 'PAPER' && computerChoice === 'SCISSORS'
+        || humanChoice === 'SCISSORS' && computerChoice === 'ROCK'
+    ) {
+        console.log(`${humanChoice} vs ${computerChoice}. You Loose!`)
+        computerScore++
+    }
+    else {
+        console.log(`${humanChoice} vs ${computerChoice}. It\'s a tie!`)
+    }
 
+    console.info('Your score: ' + humanScore)
+    console.info('Computer score: ' + computerScore)
 }
 
-console.log(getHumanChoice())
+playRound(getHumanChoice(), getComputerChoice())
