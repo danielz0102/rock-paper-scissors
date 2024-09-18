4f38fb76747db16e0a0e732a54ac4781b5188c71
danielz0102 <danyzglez@hotmail.com>
2024-09-17 19:12:50 -0600

    Add getHumanChoice()

--- script.js
+++ script.js
@@ -6,4 +6,13 @@ function getComputerChoice() {
         'Scissors'
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let choice = +prompt('Enter a number between 1 and 3')
+
+    return choice === 1 ? 'Rock' :
+        choice === 2 ? 'Paper' :
+        choice === 3 ? 'Scissors' :
+        'Sorry, that\'s not correct. You have to enter a number between 1 and 3.'
+}
+
+console.info(getHumanChoice())
