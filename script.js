51c0f3f51129c8d4ac9a31cad1ddcdfa092c8f3f
danielz0102 <danyzglez@hotmail.com>
2024-09-17 19:05:25 -0600

    Add getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3)
+    
+    return choice === 0 ? 'Rock' :
+        choice === 1 ? 'Paper' :
+        'Scissors'
+}
+
+console.log(getComputerChoice())
