ebbf350c0ec5a36c4f58281a2e4faaa227579b4b
danielz0102 <danyzglez@hotmail.com>
2024-09-19 18:00:30 -0600

    Add logic to play 5 rounds

--- script.js
+++ script.js
@@ -47,13 +47,49 @@ function playRound(humanChoice, computerChoice) {
     }
 
     console.info('Your score: ' + humanScore)
-    console.info('Computer score: ' + computerScore)
+    console.info('Bot score: ' + computerScore)
 }
 
-let humanChoice = getHumanChoice()
+function playGame() {
+    let rounds = 1
+    const LAST_ROUND = 5
 
-if (humanChoice === 'ROCK' || humanChoice === 'PAPER' || humanChoice === 'SCISSORS') {
-    playRound(humanChoice, getComputerChoice())
-} else {
-    console.log('The game was cancelled')
+    while (rounds <= 5) {
+        let humanChoice = getHumanChoice()
+        
+        if (humanChoice === 'ROCK' || humanChoice === 'PAPER' || humanChoice === 'SCISSORS') {
+            playRound(humanChoice, getComputerChoice())
+        } else {
+            break
+        }
+        
+        if (rounds === 5) {
+            break
+        }
+        rounds++
+    }
+    
+    if (rounds === LAST_ROUND) {
+        showWinner()
+    } else {
+        console.log('The game was cancelled')
+    }
 }
+
+function showWinner() {
+    const score = `
+    You: ${humanScore}
+    Bot: ${computerScore}
+    `
+
+    if (humanScore < computerScore) {
+        console.log(score + 'You loose :\'(')
+    }
+    else if (humanScore > computerScore) {
+        console.log(score + 'You Win! :D')
+    } else {
+        console.log(score + 'It\'s a tie!')
+    }
+}
+
+playGame()
