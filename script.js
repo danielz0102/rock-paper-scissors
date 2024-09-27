14f69d870d5f700d5625a1e25f488007a06ecf91
danielz0102 <danyzglez@hotmail.com>
2024-09-27 00:18:28 -0600

    Refactor to play 5 rounds

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
-let humanScore = 0, computerScore = 0
+let humanScore = 0, computerScore = 0, round = 1
 
 const rockBtn = document.querySelector('#rock')
 const paperBtn = document.querySelector('#paper')
 const scissorsBtn = document.querySelector('#scissors')
 const resultContainer = document.querySelector('#result')
-const combat = document.createElement('p')
+const result = document.createElement('p')
 const playerScore = document.createElement('p')
 const botScore = document.createElement('p')
 
-resultContainer.appendChild(combat)
+resultContainer.appendChild(result)
 resultContainer.appendChild(playerScore)
 resultContainer.appendChild(botScore)
 
 document.body.addEventListener('click', e => {
-    if (e.target === rockBtn) playRound('ROCK', getComputerChoice())
-    if (e.target === paperBtn) playRound('PAPER', getComputerChoice())
-    if (e.target === scissorsBtn) playRound('SCISSORS', getComputerChoice())
+    let playerChoice
+
+    playerChoice = (e.target === rockBtn ? 'ROCK' :
+        e.target === paperBtn ? 'PAPER' :
+        'SCISSORS'
+    )
+    
+    playRound(playerChoice, getComputerChoice())
 })
+
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3)
     
-    return choice === 0 ? 'ROCK' :
+    return (choice === 0 ? 'ROCK' :
         choice === 1 ? 'PAPER' :
         'SCISSORS'
-}
-
-function getHumanChoice() {
-    let choice = prompt('Enter an option: Rock, Paper or Scissors')
-
-    if (choice !== null) {
-        choice = choice.toUpperCase().trim()
-
-        if (choice !== 'ROCK' && choice !== 'PAPER' && choice !== 'SCISSORS') {
-            console.error('Sorry, that\'s not correct. Please, choice rock, paper or scissors.')
-            choice = getHumanChoice()
-        }
-    }
-
-    return choice
+    )
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -50,36 +42,36 @@ function playRound(humanChoice, computerChoice) {
         || humanChoice === 'PAPER' && computerChoice === 'ROCK'
         || humanChoice === 'SCISSORS' && computerChoice === 'PAPER'
     ) {
-        combat.textContent = `${humanChoice} vs ${computerChoice}. You Win!`
+        result.textContent = `${humanChoice} vs ${computerChoice}. You Win!`
         humanScore++
     }
     else if (humanChoice === 'ROCK' && computerChoice === 'PAPER'
         || humanChoice === 'PAPER' && computerChoice === 'SCISSORS'
         || humanChoice === 'SCISSORS' && computerChoice === 'ROCK'
     ) {
-        combat.textContent = `${humanChoice} vs ${computerChoice}. You Loose!`
+        result.textContent = `${humanChoice} vs ${computerChoice}. You Loose!`
         computerScore++
     }
     else {
-        combat.textContent = `${humanChoice} vs ${computerChoice}. It's a tie!`
+        result.textContent = `${humanChoice} vs ${computerChoice}. It's a tie!`
     }
 
     playerScore.textContent = 'Your score: ' + humanScore
     botScore.textContent = 'Bot score: ' + computerScore
+
+    round++
+    if (round > 5) showWinner()
 }
 
 function showWinner() {
-    const score = `
-    You: ${humanScore}
-    Bot: ${computerScore}
-    `
+    result.textContent = 'The game is over!'
 
     if (humanScore < computerScore) {
-        console.log(score + 'You loose :\'(')
+        result.textContent += ' ... you loose :('
     }
     else if (humanScore > computerScore) {
-        console.log(score + 'You Win! :D')
+        result.textContent += ' YOU WIN!'
     } else {
-        console.log(score + 'It\'s a tie!')
+        result.textContent += ' IT\'S A TIE!'
     }
 }
